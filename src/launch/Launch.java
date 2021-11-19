12bf8b627f89e8cd5fc233429fadcb645132f249
B3njam.in <41966770+Benjamin158@users.noreply.github.com>
2021-11-19 19:14:08 +0100

    Add files via upload

---
+++ src/launch/Launch.java
@@ -0,0 +1,22 @@
+package launch;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.Parent;
+
+import java.io.IOException;
+
+public class Launch extends Application{
+    public void start(Stage stage) throws IOException {
+        Parent root=(Parent) FXMLLoader.load(getClass().getClassLoader().getResource("mario.fxml"));
+        Scene scene=new Scene(root,400,400);
+        stage.setScene(scene);
+        stage.show();
+    }
+    public static void main(String[] args){
+        launch(args);
+    }
+}
